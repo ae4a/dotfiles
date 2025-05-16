ca4a6686f3a3e6726f68170ebb27f7827e56c8dd
Andrey Egorov <and7643@gmail.com>
2025-05-16 12:22:39 +0300

    feat: minimal

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+killall -q polybar # Отключение бара, если он включен
+echo "---" | tee -a /tmp/example_bar.log # Просмотр логов
+polybar example >> /tmp/example_bar.log # Запуск бара example и запись его лога
