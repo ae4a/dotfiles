61165e57634a233e0c33f1d68055ba8299a02fdf
Andrey Egorov <and7643@gmail.com>
2025-05-17 18:15:36 +0300

    chore: polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,9 +2,9 @@
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
+    MONITOR=$m polybar --reload main &
   done
 else
-  polybar --reload example &
+  polybar --reload main &
 fi
 
