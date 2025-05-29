6c5a2ad28119b9711e0c7135d602232239f777d8
Andrey Egorov <and7643@gmail.com>
2025-05-29 22:21:23 +0300

    chore: gpg test 4

--- .config/hypr/scripts/monitor.sh
+++ .config/hypr/scripts/monitor.sh
@@ -106,4 +106,3 @@ handleExisting
 # Handle changes
 echo "starting linstening to socket"
 socat -U - UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock | while read -r line; do handle "$line"; done
-
